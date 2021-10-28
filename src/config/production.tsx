c4b74af3ef1abb7863ceddb1a7dfe11798810872
Adam Venord <avenord.dev@gmail.com>
2021-10-28 09:02:36 +0800

    Add publishable key for Stripe

--- src/config/production.tsx
+++ src/config/production.tsx
@@ -26,7 +26,7 @@ const tenantMode = 'multi';
  * Plan payments configuration.
  */
 const isPlanEnabled = true;
-const stripePublishableKey = '';
+const stripePublishableKey = 'pk_test_51JpIyQLnyNFZ6q7AEmHZJN6WgBTYMH6vuKmAgXxUPirX9TFm6YbiUH2GmeRh5xGcSpMaD3DNm8iDYxNIxObigfWt00alQ3BPmS';
 
 export default {
   frontendUrl,
