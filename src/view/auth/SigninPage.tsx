e89a8adfac007781091bbe40c1ea198bca7ab59e
headline-design <aaron@headline-inc.com>
2021-10-31 23:38:51 +0000

    update css-2

--- src/view/auth/SigninPage.tsx
+++ src/view/auth/SigninPage.tsx
@@ -20,6 +20,8 @@ import config from 'src/config';
 import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import Message from 'src/view/shared/message';
+import BackgroundVideo from 'src/components/BackgroundVideo/BackgroundVideo';
+import 'src/app.css';
 
 const schema = yup.object().shape({
   email: yupFormSchemas.string(i18n('user.fields.email'), {
@@ -36,6 +38,7 @@ const schema = yup.object().shape({
   ),
 });
 
+const videoSource = "/assets/video.mp4";
 function SigninPage() {
   const location = useLocation();
   const dispatch = useDispatch();
@@ -94,15 +97,16 @@ function SigninPage() {
   };
 
   return (
-    <Wrapper 
-      style={{
-        backgroundImage: `url(${
-          backgroundImageUrl || '/images/signin.jpg'
-        })`,
-      }}
-    >
-      <Content >
+    <Wrapper className="screenshot" >      
+      <BackgroundVideo
+        blur={0}
+        videoSource={videoSource} >
+      </BackgroundVideo>
+      <Content className="screenshot-2">
              <Logo>
+             <a className="navbar-brand" href="."><h1 className="logo-font"><strong>Algo</strong>Cloud</h1><svg className="svg-logo" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
+  <path d="M8.5 4a4.002 4.002 0 0 0-3.8 2.745.5.5 0 1 1-.949-.313 5.002 5.002 0 0 1 9.654.595A3 3 0 0 1 13 13H.5a.5.5 0 0 1 0-1H13a2 2 0 0 0 .001-4h-.026a.5.5 0 0 1-.5-.445A4 4 0 0 0 8.5 4zM0 8.5A.5.5 0 0 1 .5 8h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"></path>
+</svg></a>
           {logoUrl ? (
             <img
               src={logoUrl}
@@ -110,7 +114,7 @@ function SigninPage() {
               alt={i18n('app.title')}
             />
           ) : (
-            <h1>{i18n('app.title')}</h1>
+            <h1></h1>
           )}
         </Logo>
         <FormProvider {...form}>
@@ -206,6 +210,7 @@ function SigninPage() {
         </FormProvider>
       </Content>
     </Wrapper>
+    
   );
 }
 
