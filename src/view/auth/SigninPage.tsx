5d0c0dd236a88f6e292e23c5c5e89465f1e25060
headline-design <aaron@headline-inc.com>
2021-11-02 07:01:56 +0000

    CS

--- src/view/auth/SigninPage.tsx
+++ src/view/auth/SigninPage.tsx
@@ -104,9 +104,7 @@ function SigninPage() {
       </BackgroundVideo>
       <Content className="screenshot-2">
              <Logo>
-             <a className="screenshot-navbar-brand" href="."><h1 className="logo-font"><strong>Algo</strong>Cloud</h1><svg className="svg-logo" xmlns="http://www.w3.org/2000/svg" width="50" height="48" fill="currentColor" viewBox="0 0 16 16">
-  <path d="M8.5 4a4.002 4.002 0 0 0-3.8 2.745.5.5 0 1 1-.949-.313 5.002 5.002 0 0 1 9.654.595A3 3 0 0 1 13 13H.5a.5.5 0 0 1 0-1H13a2 2 0 0 0 .001-4h-.026a.5.5 0 0 1-.5-.445A4 4 0 0 0 8.5 4zM0 8.5A.5.5 0 0 1 .5 8h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"></path>
-</svg></a>
+             <a className="algocloud-navbar-brand-landing" href="."><div className="algocloud-font-landing"><strong>Algo</strong>Cloud</div><svg xmlns="http://www.w3.org/2000/svg" width="28" height="22" fill="currentColor" viewBox="-2 3 18 12" className="algocloud-logo-landing"><path d="M8.5 4a4.002 4.002 0 0 0-3.8 2.745.5.5 0 1 1-.949-.313 5.002 5.002 0 0 1 9.654.595A3 3 0 0 1 13 13H.5a.5.5 0 0 1 0-1H13a2 2 0 0 0 .001-4h-.026a.5.5 0 0 1-.5-.445A4 4 0 0 0 8.5 4zM0 8.5A.5.5 0 0 1 .5 8h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"></path></svg></a>
           {logoUrl ? (
             <img
               src={logoUrl}
