0211fddfd85e3b71790341538219f8bcf98b1aba
Josem Demira <jdemira130@gmail.com>
2021-11-05 17:39:48 -0700

    dark theme dashboard

--- src/view/dashboard/DashboardBarChart.tsx
+++ src/view/dashboard/DashboardBarChart.tsx
@@ -12,9 +12,9 @@ const data = {
   ],
   datasets: [
     {
-      label: i18n('dashboard.charts.red'),
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
+      label: i18n('dashboard.chats.rred'),
+      backgroundColor: 'rgba(205, 67, 103, 1)',
+      borderColor: 'rgba(205, 67, 103, 1)',
       borderWidth: 1,
       hoverBackgroundColor: 'rgba(255,99,132,0.4)',
       hoverBorderColor: 'rgba(255,99,132,1)',
