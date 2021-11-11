a847f2c4e671b3fe38767620affca1cdbc08be8f
Adam Venord <avenord.dev@gmail.com>
2021-11-11 08:56:43 +0800

    Add Breadcrumb to TenantListPage

--- src/view/superadmin/tenant/list/TenantListPage.tsx
+++ src/view/superadmin/tenant/list/TenantListPage.tsx
@@ -8,12 +8,19 @@ import TenantListToolbar from 'src/view/superadmin/tenant/list/TenantListToolbar
 import ContentWrapper from 'src/view/layout/styles/ContentWrapper';
 import PageTitle from 'src/view/shared/styles/PageTitle';
 import { i18n } from 'src/i18n';
+import Breadcrumb from 'src/view/shared/Breadcrumb';
 
 function TenantListPage(props) {
 
   return (
     <>
-      <ContentWrapper style={{ marginTop: '0px' }}>
+      <Breadcrumb
+        items={[
+          [i18n('dashboard.menu'), '/'],
+          [i18n('tenant.list.menu')],
+        ]}
+      />
+      <ContentWrapper>
         <PageTitle>{i18n('tenant.list.title')}</PageTitle>
 
         <TenantListToolbar />
