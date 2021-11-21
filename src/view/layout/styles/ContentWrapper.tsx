28afb7990c819ec2fadc1359b7fd1cc165170be5
headline-design <aaron@headline-inc.com>
2021-11-21 08:04:19 +0000

    dark-mode

--- src/view/layout/styles/ContentWrapper.tsx
+++ src/view/layout/styles/ContentWrapper.tsx
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 const ContentWrapper = styled.div`
   margin-top: 16px;
   padding: 24px;
-  background-color: rgba(255,255,255,0.05);
+  background: #1e2d58; 
+  background: rgba(255,255,255,.05); 
+  box-shadow: 0 0 50px 14px rgb(18 28 53 / 3%); 
+  border: 1px solid;
+  border-color: var(--border-var) !important;
   width: 100%;
   border-radius: 5px;
-  border: 1px solid #dee2e6 !important;
 `;
 
 export default ContentWrapper;
