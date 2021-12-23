48c2eb05b2dabdd26b0ebe4a9f15345cb3847e5a
Adam Venord <avenord.dev@gmail.com>
2021-12-24 03:27:52 +0800

    rename PoolsTable component

--- src/view/algorand/pages/pools/PoolListPage.tsx
+++ src/view/algorand/pages/pools/PoolListPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { i18n } from 'src/i18n';
-import PoolsTable from 'src/view/algorand/pages/pools/poolsTable';
+import PoolsTable from 'src/view/algorand/pages/pools/PoolsTable';
 import ContentWrapper from 'src/view/layout/styles/ContentWrapper';
 import Breadcrumb from 'src/view/shared/Breadcrumb';
 import PageTitle from 'src/view/shared/styles/PageTitle';
