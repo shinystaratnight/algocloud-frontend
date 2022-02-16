19c5a0ada69bc8e3acb05e3420fea0ef1655ed1b
headline-design <aaron@headline-inc.com>
2022-02-16 12:17:36 +0000

    toast style

--- src/view/shared/message.tsx
+++ src/view/shared/message.tsx
@@ -1,21 +1,25 @@
 import * as toastr from 'toastr';
-import 'toastr/build/toastr.css';
+
 
 export default class Message {
   static success(arg) {
     // must be changed change because Bootstrap UI
     // css overrides the style
-    toastr.options.toastClass = 'toastr';
-    toastr.options.positionClass = 'toast-bottom-left';
+    toastr.options.toastClass = 'me-3 fas fa-check-circle-2 text-white fs-3';
+    toastr.options.positionClass = 'alert alert-success bg-success border-2 d-flex align-items-center toast-bottom-left';
+    <button className="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+
 
     toastr.success(arg);
   }
 
+  
+ 
   static error(arg) {
     // must be changed change because Bootstrap UI
     // css overrides the style
-    toastr.options.toastClass = 'toastr';
-    toastr.options.positionClass = 'toast-bottom-left';
+    toastr.options.toastClass = 'me-3 fas fa-times-circle text-white fs-3';
+    toastr.options.positionClass = 'alert alert-danger bg-danger border-2 d-flex align-items-center toast-bottom-left';
 
     toastr.error(arg);
   }
