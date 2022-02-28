3e86319f0a18dba3529db93e90e201d8a58ec7b4
Adam Venord <avenord.work@gmail.com>
2022-02-28 17:23:38 -0500

    OverviewChat page some code format

--- src/view/algorand/pages/overview/chart/OverviewChart.tsx
+++ src/view/algorand/pages/overview/chart/OverviewChart.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { ResponsiveContainer } from 'recharts';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import selectors from 'src/modules/algorand/overview/overviewSelectors';
 import { CHART_TYPES } from 'src/modules/algorand/constants';
 import ContentWrapper from 'src/view/layout/styles/ContentWrapper';
@@ -31,12 +31,12 @@ function ShowcaseChart() {
   const priceData = useSelector(selectors.selectHourlyPrices);
   let image = showcase['assetId'] === 0 ? '/assets/asa-list/ALGO/icon.png' : `https://algoexplorer.io/images/assets/big/light/${showcase['assetId']}.png`;
   for (let i = 0; i < images?.length; i++) {
-      const img = images[i];
-      let id = parseInt(img.split('-')[1]);
-      if (id == showcase['assetId']) {
-          image = `/assets/asa-list/${img}/icon.png`;
-          break;
-      }
+    const img = images[i];
+    let id = parseInt(img.split('-')[1]);
+    if (id == showcase['assetId']) {
+      image = `/assets/asa-list/${img}/icon.png`;
+      break;
+    }
   }
 
   useEffect(() => {
@@ -58,56 +58,47 @@ function ShowcaseChart() {
     }
   }, [ref1, ref2, ref3, ref4])
 
-  return ( 
+  return (
     <FlexContainer gap="20px" className="showcase-row">
-                  <div className="container w-50 w-100 card bg-box rounded card-hover-3 m-0 p-0">
-                <div className="ol" style={{ maxWidth: "100%", alignItems: "center" }}> <div className="p-3 token-card" style={{ width: "min-content" }}><img className='token card-token' src={image} style={{ width: 60}}></img></div>
-                <div className="p-2  col-sm w-130">
-                      <h5 className="banner-ticker" >{showcase['unitName']}</h5>
-                    <Link to={`/algorand/assets/${showcase.assetId}`}>
- 
-                    <h6 className='ww'>{showcase.name}</h6></Link> 
+      <div className="container w-50 w-100 card bg-box rounded card-hover-3 m-0 p-0">
+        <div className="ol" style={{ maxWidth: "100%", alignItems: "center" }}> <div className="p-3 token-card" style={{ width: "min-content" }}><img className='token card-token' src={image} style={{ width: 60 }}></img></div>
+          <div className="p-2  col-sm w-130">
+            <h5 className="banner-ticker" >{showcase['unitName']}</h5>
+            <Link to={`/algorand/assets/${showcase.assetId}`}>
 
-                  </div>
-                <div className="p-2  col-sm w-130">
-                        <h5 className='text-info banner-ticker' >{(showcase.price)?.toFixed(4) || ''}</h5>
-                        <h6 className="ww">Price (Live)</h6></div>
-                <div className="p-2  col-sm w-130">
-                        <h5 className={showcase.lastDayPriceChange ? 'text-danger ww' : 'text-success ww'}> {(showcase.lastDayPriceChange) ? (parseFloat(formatPercent(showcase.lastDayPriceChange)) < 0) ? <span className="ww-1">{'  '}<div  >{(showcase.lastDayPriceChange)?.toFixed(3)  || ''}</div><i
-                            className={`fas fa-arrow-down pl-2`}
-                        ></i></span> : <span>{'  '}<i
-                            className={`fas fa-arrow-up pl-2`}
-                        ></i></span> : ''}</h5>
-                        <h6 className="ww">Price (24hrs)</h6></div>
-                <div className="p-2  col-sm w-130">
-                        <h5 className={showcase.lastDayLiquidityChange ? 'text-danger ww' : 'text-success ww'}> {(showcase.lastDayLiquidityChange) ? (parseFloat(formatPercent(showcase.llastDayLiquidityChange)) < 0) ? <span className="ww-1">{'  '}<div  >{(showcase.lastDayLiquidityChange)?.toFixed(3)  || ''}</div><i
-                            className={`fas fa-arrow-down pl-2`}
-                        ></i></span> : <span>{'  '}<i
-                            className={`fas fa-arrow-up pl-2`}
-                        ></i></span> : ''}</h5>
-                        <h6 className="ww">Liquidity (24hrs)</h6></div>
-                <div className="p-2  col-sm w-130">
-                        <h5 className={showcase.lastDayVolumeChange ? 'text-danger ww' : 'text-success ww'}> {(showcase.lastDayVolumeChange) ? (parseFloat(formatPercent(showcase.lastDayVolumeChange)) < 0) ? <span className="ww-1">{'  '}<div  >{(showcase.lastDayVolumeChange)?.toFixed(3)  || ''}</div><i
-                            className={`fas fa-arrow-down pl-2`}
-                        ></i></span> : <span>{'  '}<i
-                            className={`fas fa-arrow-up pl-2`}
-                        ></i></span> : ''}</h5>
-                        <h6 className="ww">Volume (24hrs)</h6></div>
-                        </div></div>
-                 
+              <h6 className='ww'>{showcase.name}</h6></Link>
 
-                    
-
-
-
-
-
-                    
-                    
-                    
-      
-     
-                      
+          </div>
+          <div className="p-2  col-sm w-130">
+            <h5 className='text-info banner-ticker' >{(showcase.price)?.toFixed(4) || ''}</h5>
+            <h6 className="ww">Price (Live)</h6>
+          </div>
+          <div className="p-2  col-sm w-130">
+            <h5 className={showcase.lastDayPriceChange ? 'text-danger ww' : 'text-success ww'}> {(showcase.lastDayPriceChange) ? (parseFloat(formatPercent(showcase.lastDayPriceChange)) < 0) ? <span className="ww-1">{'  '}<div  >{(showcase.lastDayPriceChange)?.toFixed(3) || ''}</div><i
+              className={`fas fa-arrow-down pl-2`}
+            ></i></span> : <span>{'  '}<i
+              className={`fas fa-arrow-up pl-2`}
+            ></i></span> : ''}</h5>
+            <h6 className="ww">Price (24hrs)</h6>
+          </div>
+          <div className="p-2  col-sm w-130">
+            <h5 className={showcase.lastDayLiquidityChange ? 'text-danger ww' : 'text-success ww'}> {(showcase.lastDayLiquidityChange) ? (parseFloat(formatPercent(showcase.llastDayLiquidityChange)) < 0) ? <span className="ww-1">{'  '}<div  >{(showcase.lastDayLiquidityChange)?.toFixed(3) || ''}</div><i
+              className={`fas fa-arrow-down pl-2`}
+            ></i></span> : <span>{'  '}<i
+              className={`fas fa-arrow-up pl-2`}
+            ></i></span> : ''}</h5>
+            <h6 className="ww">Liquidity (24hrs)</h6>
+          </div>
+          <div className="p-2  col-sm w-130">
+            <h5 className={showcase.lastDayVolumeChange ? 'text-danger ww' : 'text-success ww'}> {(showcase.lastDayVolumeChange) ? (parseFloat(formatPercent(showcase.lastDayVolumeChange)) < 0) ? <span className="ww-1">{'  '}<div  >{(showcase.lastDayVolumeChange)?.toFixed(3) || ''}</div><i
+              className={`fas fa-arrow-down pl-2`}
+            ></i></span> : <span>{'  '}<i
+              className={`fas fa-arrow-up pl-2`}
+            ></i></span> : ''}</h5>
+            <h6 className="ww">Volume (24hrs)</h6>
+          </div>
+        </div>
+      </div>
       <div className="showcase-row-1">
         <ContentWrapper gap="10px" className="w-50 w-100 card bg-box rounded card-hover m-0">
           <ResponsiveContainer aspect={40 / 28} ref={ref1}>
@@ -123,7 +114,6 @@ function ShowcaseChart() {
             />
           </ResponsiveContainer>
         </ContentWrapper>
-
         <ChartWindowWrapper gap="10px" className="w-50 w-100 card bg-box rounded card-hover m-0">
           <ResponsiveContainer aspect={40 / 28} ref={ref2}>
             <TradingViewChart
@@ -143,17 +133,6 @@ function ShowcaseChart() {
       <div className="showcase-row-1">
         <ChartWindowWrapper gap="10px" className="w-50 w-100 card bg-box rounded card-hover m-0">
           <ResponsiveContainer aspect={40 / 28} ref={ref3}>
-            {/* <TradingViewChart
-              data={dailyData}
-              base={0}
-              title={`<a href="/algorand/assets/${showcase.assetId}">${showcase.unitName || ''}</a> Market Cap`}
-              field={'marketCap'}
-              width={width4}
-              type={CHART_TYPES.AREA}
-              timeField='date'
-              useWeekly={false}
-              utc={true}
-            /> */}
             <CandleStickChart
               data={priceData}
               width={width3}
